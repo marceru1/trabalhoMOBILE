06342a36a0ee97d31c2c29a8407de8efca00bfb8
marcelo filho <mcfilho2002@gmail.com>
2025-05-22 23:46:34 -0300

    agora_sim

--- conn.php
+++ conn.php
@@ -2,10 +2,11 @@
 $servidor = 'localhost' ;
 $usuario = 'root' ;
 $senha = '';
-$banco = 'biblioteca' ;
+$banco = 'produtos_db' ;
 $conn = mysqli_connect($servidor , $usuario , $senha , $banco);
 
 if (!$conn) {
+    
     // echo "negativo";
 }else {
     // echo "positivo" ;
